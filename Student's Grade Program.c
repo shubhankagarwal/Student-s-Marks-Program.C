9e0e3d5fdcc766c2c5037b97d29f5b3c480d9422
shubhankagarwal <63141928+shubhankagarwal@users.noreply.github.com>
2020-04-05 12:32:25 +0530

    Add files via upload

--- Student's Grade Program.c
+++ Student's Grade Program.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
+float main()
 {
 	float a;
 	printf("Enter Marks Obtained=");
-	scanf("%f",&a);
-	if(a<=100 && a>=85)
+	scanf("%f",&a);                     /* To take input of the marks*/
+	if(a<=100 && a>=85)                 /* To apply the given conditions*/
     printf("Grade A");
     
-    else if(a<=84 && a>=70)
+    else if(a<=84 && a>=70)             /* To apply the given conditions*/
     printf("Grade B");
     
-    else if(a<=69 && a>=55)
+    else if(a<=69 && a>=55)             /* To apply the given conditions*/
     printf("Grade C");
     
-    else if (a<=54 && a>=40)
+    else if (a<=54 && a>=40)            /* To apply the given conditions*/
     printf("Grade D");
     
-    else if( a>=0 &&   a<40)
+    else if( a>=0 &&   a<40)            /* To apply the given conditions*/
     printf("Grade F");
     
-    else
+    else                                 /* For number entered other than marks*/
     printf("Invalid");
     
     
